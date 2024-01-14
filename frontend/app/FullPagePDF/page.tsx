bdd547983845c528337f18560b2689261cab5568
Eirik Bjørdal <eiriksbjordal@gmail.com>
2024-01-14 11:45:42 +0100

    Some small fix, will probably be changed

--- frontend/app/FullPagePDF/page.tsx
+++ frontend/app/FullPagePDF/page.tsx
@@ -17,12 +17,9 @@ export default function Page() {
     }, []);
 
     return (
-        <div>
+        <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}>
             {pdfData ? (
-                <>
-                    <p>PDF found.</p>
-                    <embed src={pdfData} type="application/pdf" width="100%" height="600px" />
-                </>
+                <embed src={pdfData} type="application/pdf" width="100%" height="100%" />
             ) : (
                 <p>No PDF found.</p>
             )}
