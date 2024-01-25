feafbc4013e92e337d208e5fec590f53de8b696a
Trivinyx <tom.a.s.myre@gmail.com>
2024-01-25 17:14:26 +0100

    Creating models.py
    making sure to import the basemodel, for type synergy with fastAPI types in routes.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
-
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
 # Test API that returns Hello World :)
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
