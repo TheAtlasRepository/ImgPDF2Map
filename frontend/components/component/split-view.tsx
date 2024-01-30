1491a3e7f88c4a3395ed4a2bf69773d4de840f13
mnilsen <32636970+ostepizza@users.noreply.github.com>
2024-01-30 12:13:57 +0100

    Resizable split view introduced
    Resizable split view works, map does however not resize properly.

--- frontend/components/component/split-view.tsx
+++ frontend/components/component/split-view.tsx
@@ -1,30 +1,35 @@
-import Map, { NavigationControl, GeolocateControl } from "react-map-gl";
+import {Map, NavigationControl, GeolocateControl } from "react-map-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'react-image-crop/dist/ReactCrop.css'
 import ImageMap from "./moveImage";
+import { Allotment } from "allotment";
+import "allotment/dist/style.css";
 
 export default function SplitView() {
   
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
   return (
-    <div className="flex h-screen">
-    <div className="w-1/2 relative" style={{ zIndex: 1}}>
-      <Map
-        mapboxAccessToken={mapboxToken}
-        mapStyle="mapbox://styles/mapbox/streets-v12"
-        initialViewState={{ latitude: 35.668641, longitude: 139.750567, zoom: 10 }}
-        maxZoom={20}
-        minZoom={3}
-      >
-        <GeolocateControl position="bottom-right" />
-        <NavigationControl position="bottom-right" />
-      </Map>
+    <div className="h-screen">
+      <Allotment>
+        <Allotment.Pane minSize={200}>
+          <Map
+            mapboxAccessToken={mapboxToken}
+            mapStyle="mapbox://styles/mapbox/streets-v12"
+            initialViewState={{ latitude: 35.668641, longitude: 139.750567, zoom: 10 }}
+            maxZoom={20}
+            minZoom={3}
+            reuseMaps={true}
+          >
+            <GeolocateControl position="bottom-right" />
+            <NavigationControl position="bottom-right" />
+          </Map>
+        </Allotment.Pane>
+        <Allotment.Pane minSize={200}>
+          <ImageMap src={localStorage.getItem('pdfData')!} />
+        </Allotment.Pane>
+      </Allotment>
     </div>
-    <div className="w-1/2 flex inset-2 justify-center items-center h-full">
-      <ImageMap src={localStorage.getItem('pdfData')!} />
-    </div>
-  </div>
   
 );
 }
