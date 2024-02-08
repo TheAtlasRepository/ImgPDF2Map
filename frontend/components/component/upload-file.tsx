ef0dca4c84c5398fb76e6863c5dc25547000d651
Sebastian <89196146+sebmid@users.noreply.github.com>
2024-02-08 11:48:10 +0100

    F sm frontend pdf page select (#28)
    Working pdfselector that shows selected pdf page and sends correct page number to backend and redirects to conversion.

--- frontend/components/component/upload-file.tsx
+++ frontend/components/component/upload-file.tsx
@@ -1,120 +1,146 @@
 import { Button } from "@/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { useSearchParams, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useSearchParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function UploadFile() {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null); // Add state for error message
+  const router = useRouter();
+  const [fileType, setFileType] = useState("");
+  const [fileName, setFileName] = useState("");
+  const params = useSearchParams();
 
-	const [errorMessage, setErrorMessage] = useState<string | null>(null); // Add state for error message
-	const router = useRouter()
-	const [fileType, setFileType] = useState('');
-	const [fileName, setFileName] = useState('');
-	const params = useSearchParams();
+  useEffect(() => {
+    if (params.get("e")) {
+      setErrorMessage(params.get("e") as string);
+    }
+  });
 
+  // Handle file input change when user has used "Open a file"-button
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      checkFileType(file);
+    }
+  };
 
-	useEffect(() => {
-		if (params.get("e")) {
-			setErrorMessage(params.get("e") as string);
-		}
-	});
+  // Handle drag over
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
 
-	// Handle file input change when user has used "Open a file"-button
-	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const file = event.target.files?.[0];
-		if (file) {
-			checkFileType(file);
-		}
-	};
+  // Handle file drop
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files?.[0];
+    if (file) {
+      checkFileType(file);
+    }
+  };
 
-	// Handle drag over
-	const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
-	};
+  // Check if file type is supported
+  const checkFileType = (file: File) => {
+    if (file) {
+      setErrorMessage(null); // Clear any existing error message
+      setFileType(file.type);
+      setFileName(file.name);
 
-	// Handle file drop
-	const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
-		const file = event.dataTransfer.files?.[0];
-		if (file) {
-			checkFileType(file);
-		}
-	};
+      // Read and save file to local storage
+      const reader = new FileReader();
+      reader.onload = () => {
+        const blob = new Blob([reader.result as string], { type: file.type });
+        localStorage.setItem(
+          "pdfData",
+          JSON.stringify({
+            url: URL.createObjectURL(blob),
+            type: blob.type,
+          })
+        );
+      };
+      reader.readAsArrayBuffer(file);
 
-	// Check if file type is supported
-	const checkFileType = (file: File) => {
-		if (file && (file.type === 'application/pdf' || file.type.startsWith('image/'))) {
-			setErrorMessage(null); // Clear any existing error message
-			setFileType(file.type);
-			setFileName(file.name);
+      // Delete previous PDF if it exists
+      if (localStorage.getItem("pdfData")) {
+        URL.revokeObjectURL(localStorage.getItem("pdfData")!);
+        localStorage.removeItem("pdfData");
+      }
 
-			// Read and save file to local storage
-			const reader = new FileReader();
-			reader.onload = () => {
-				const blob = new Blob([reader.result as string], { type: file.type });
-				localStorage.setItem('pdfData', JSON.stringify({
-					url: URL.createObjectURL(blob),
-					type: blob.type
-				}));
-			};
-			reader.readAsArrayBuffer(file);
+      // Route based on file type
+      if (file.type === "application/pdf") {
+        // Push to PdfSelect, where the PDF will be displayed
+        router.push("/PdfSelect");
+      } else if (file.type.startsWith("image/")) {
+        // Push to Conversion, where the image will be converted to PNG
+        router.push("/Conversion");
+      }
+    } else {
+      setErrorMessage("File type not supported.");
+    }
+  };
 
-			// Delete previous PDF if it exists
-			if (localStorage.getItem('pdfData')) {
-				URL.revokeObjectURL(localStorage.getItem('pdfData')!);
-				localStorage.removeItem('pdfData');
-			}
+  return (
+    <div className="mx-auto mt-10 max-w-xl">
+      <div
+        onClick={() => document.querySelector("input")?.click()}
+        className="cursor-pointer"
+      >
+        <div
+          className="rounded-lg border-4 border-dashed border-blue-200 p-10 py-20 text-center"
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        >
+          <FolderPlusIcon className="mx-auto mb-6 text-blue-500 text-8xl" />
+          <div className="text-lg font-medium text-gray-400">
+            Open, or drop your <b>image or PDF</b> here
+          </div>
+          {fileName && (
+            <div className="text-lg font-medium text-gray-400 mt-4">
+              {fileName}
+            </div>
+          )}
+        </div>
 
-			//Push to Conversion, where the file will be converted to PNG
-			router.push('/Conversion');
-			
-		} else {
-			setErrorMessage('File type not supported.');
-		}
-	}
+        <input
+          type="file"
+          accept=".pdf, image/*"
+          onChange={handleFileChange}
+          className="hidden"
+        />
+        <Button
+          className="mt-6 w-full bg-blue-600 text-white text-xl"
+          variant="blue"
+        >
+          Open a file
+        </Button>
+      </div>
 
-	return (
-		<div className="mx-auto mt-10 max-w-xl">
-			<div onClick={() => document.querySelector('input')?.click()} className="cursor-pointer">
-				<div className="rounded-lg border-4 border-dashed border-blue-200 p-10 py-20 text-center" onDragOver={handleDragOver} onDrop={handleDrop}>
-					<FolderPlusIcon className="mx-auto mb-6 text-blue-500 text-8xl" />
-					<div className="text-lg font-medium text-gray-400">Open, or drop your <b>image or PDF</b> here</div>
-					{fileName && <div className="text-lg font-medium text-gray-400 mt-4">{fileName}</div>}
-				</div>
-
-				<input type="file" accept=".pdf, image/*" onChange={handleFileChange} className="hidden" />
-				<Button className="mt-6 w-full bg-blue-600 text-white text-xl" variant="blue">Open a file</Button>
-			</div>
-			
-
-			{errorMessage &&
-				<Alert variant="destructive" className="mt-5">
-					<AlertTitle>Error</AlertTitle>
-					<AlertDescription>
-						{errorMessage}
-					</AlertDescription>
-				</Alert>
-			}
-		</div>
-	)
+      {errorMessage && (
+        <Alert variant="destructive" className="mt-5">
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{errorMessage}</AlertDescription>
+        </Alert>
+      )}
+    </div>
+  );
 }
 
 function FolderPlusIcon(props: any) {
-	return (
-		<svg
-			{...props}
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 24"
-			fill="none"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-		>
-			<path d="M4 20h16a2 2 0 2-2V8a2 0-2-2h-7.93a2 1-1.66-.9l-.82-1.2A2 7.93 3H4a2 0-2 2v13c0 1.1.9 2Z" />
-			<line x1="12" x2="12" y1="10" y2="16" />
-			<line x1="9" x2="15" y1="13" y2="13" />
-		</svg>
-	)
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M4 20h16a2 2 0 2-2V8a2 0-2-2h-7.93a2 1-1.66-.9l-.82-1.2A2 7.93 3H4a2 0-2 2v13c0 1.1.9 2Z" />
+      <line x1="12" x2="12" y1="10" y2="16" />
+      <line x1="9" x2="15" y1="13" y2="13" />
+    </svg>
+  );
 }
