45f8de7fa9ad506035b000a75857e912d2dd0936
mnilsen <32636970+ostepizza@users.noreply.github.com>
2024-02-22 17:07:48 +0100

    Restructured Upload
    Created a single UploadPipeline component, which takes the file from upload and until it goes to the editor
    Converted Conversion page fully to a Component
    Removed the pages used earlier for Conversion and PdfSelect
    Prettified the PDF selector
    Removed some unused vars etc

---
+++ frontend/components/component/uploadPipeline.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import UploadFile from '@/components/component/upload-file';
+import PdfSelect from '@/components/component/pdfSelector';
+import Conversion from '@/components/component/conversion';
+
+function UploadPipeline() {
+    const [dataUrl, setDataUrl] = useState<string | null>(null);
+    const [fileType, setFileType] = useState<string | null>(null);
+    const [selectedPage, setSelectedPage] = useState<number | null>(null);
+
+    // Clear the states, this will hide the components and show the upload file component
+    const clearStates = () => {
+        setDataUrl(null);
+        setFileType(null);
+        setSelectedPage(null);
+    }
+
+    // On file upload, set the dataUrl and fileType
+    const handleFileUpload = (uploadedDataUrl: string, uploadedFileType: string) => {
+        setDataUrl(uploadedDataUrl);
+        setFileType(uploadedFileType);
+    };
+
+    // On page selected, set the selectedPage
+    const handlePageSelected = (pageNumber: number) => {
+        setSelectedPage(pageNumber);
+    };
+
+    return (
+        <div>
+            {
+                // If there is no fileType, show the upload file component
+                !fileType && 
+                <UploadFile 
+                    onFileUpload={handleFileUpload} 
+                />
+            }
+            {
+                // If there is a fileType and it is a PDF, show the PDF selector component
+                !selectedPage && fileType === 'application/pdf' && 
+                <PdfSelect 
+                    fileUrl={dataUrl} 
+                    onPageSelected={handlePageSelected} 
+                    clearStateRequest={clearStates}
+                />
+            }
+            {
+                // If there is a fileType and it is an image or there is a selected page, show the conversion component
+                fileType && ((fileType.startsWith('image/') || selectedPage) && 
+                <Conversion 
+                    fileUrl={dataUrl} 
+                    fileType={fileType} 
+                    pageNumber={selectedPage} 
+                    clearStateRequest={clearStates}
+                />)
+            }
+        </div>
+    );
+}
+
+export default UploadPipeline;
