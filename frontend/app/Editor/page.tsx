96484753997be8b186d98a80fbc499dc61ca6bc0
mnilsen <32636970+ostepizza@users.noreply.github.com>
2024-03-05 09:31:48 +0100

    Editor now redirects if image can not be loaded (#51)

--- frontend/app/Editor/page.tsx
+++ frontend/app/Editor/page.tsx
@@ -3,23 +3,33 @@ import { useRouter } from 'next/navigation'
 import { useState, useEffect } from 'react';
 import Editor from '../../components/component/editor';
 
-// This will be a component in the future
-
 export default function Page() {
     const router = useRouter();
-
-    // Initialize pdfData state variable
     const [pdfData, setPdfData] = useState<string | null>(null);
-    const pdfDataFromLocalStorage = window.localStorage.getItem('pdfData');
 
     useEffect(() => {
         // If pdfData is not null, set the pdfData state variable to the value of the pdfData key in localStorage
         if (typeof window !== 'undefined') {
+            const pdfDataFromLocalStorage = window.localStorage.getItem('pdfData');
             setPdfData(pdfDataFromLocalStorage !== null ? pdfDataFromLocalStorage : null);
+
+            if (pdfDataFromLocalStorage) {
+                const image = new Image();
+
+                image.onerror = () => {
+                    // If the image cannot be loaded, redirect to the home page
+                    router.push('/');
+                };
+
+                // Set the src of the image to start loading it. 
+                //If the image is not found, the onerror event will be triggered, and the user will be redirected to the home page
+                image.src = pdfDataFromLocalStorage;
+            } else {
+                // If pdfData is null, redirect to the home page
+                router.push('/');
+            }
         }
     }, []);
-      
-      
 
     return (
         <main>
@@ -27,5 +37,3 @@ export default function Page() {
         </main>
     );
 }
-
-
