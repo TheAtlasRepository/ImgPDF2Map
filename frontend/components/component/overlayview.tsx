bfd6797a16b8b17f2e82fa4bdf95f436caf8483d
Lukas Aubert Andersen <>
2024-03-12 12:10:09 +0100

    Progress on fetch and display georeferenced image

--- frontend/components/component/overlayview.tsx
+++ frontend/components/component/overlayview.tsx
@@ -9,21 +9,35 @@ interface MapOverlayProps {
 const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 const OverlayView = ({projectId}: MapOverlayProps) => {
-  const  [mapImageUrl, setMapImageUrl] = useState(null);
+  const  [tiffBlob, setTiffBlob] = useState(null || new Blob());
+  const [tiffUrl, setTiffUrl] = useState("");
 
 
   useEffect(() => {
-
     const fetchGeorefImage = async () => {
       try {
-        const response = await axios.get(`/localhost:8000/projects/${projectId}/georef-image`);
-        setMapImageUrl(response.data);
+        const apiUrl = `http://localhost:8000/project/${projectId}/image/geo`;
+        const response = await axios.get(apiUrl, {
+          responseType: 'blob', // This tells Axios to expect a blob response
+        });
+        // Since response.data contains the blob, you can directly create a blob URL from it
+        const url = URL.createObjectURL(response.data);
+        setTiffUrl(url); // Set the blob URL for use in the <img> or as a source for the Map component
+  
       } catch (error) {
         console.error("Error fetching georeferenced image:", error);
       }
     };
+  
     fetchGeorefImage();
-  }, []);
+  
+    // Clean up function to revoke the blob URL when the component unmounts
+    return () => {
+      if (tiffUrl) {
+        URL.revokeObjectURL(tiffUrl);
+      }
+    };
+  }, [projectId]); // Include projectId in the dependency array to refetch the image if it changes
 
   return (
     <Map
@@ -36,11 +50,12 @@ const OverlayView = ({projectId}: MapOverlayProps) => {
       mapStyle="mapbox://styles/mapbox/streets-v11"
       mapboxAccessToken={mapboxToken}
     >
-      {mapImageUrl && (
+      {tiffUrl && (
         <Source
           id="georeferenced-image-source"
           type="image"
-          url={mapImageUrl}
+          url={tiffUrl}
+          coordinates=
         >
           <Layer
             id="georeferenced-image-layer"
