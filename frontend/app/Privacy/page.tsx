7ed90132765310b54f7051fd3d7aecd6ab8d3e49
mnilsen <32636970+ostepizza@users.noreply.github.com>
2024-03-26 12:50:27 +0100

    Data Policy added

--- frontend/app/Privacy/page.tsx
+++ frontend/app/Privacy/page.tsx
@@ -15,7 +15,7 @@ export default function About() {
             </h1>
           <div className="w-100 p-6 card dark:bg-gray-800">
             <p className="text-lg text-secondary dark:text-gray-300">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in dapibus neque. Vestibulum vitae nibh vel nibh faucibus elementum gravida eget augue. Cras fringilla iaculis interdum. Vivamus eu pellentesque felis, a blandit nibh. Suspendisse hendrerit varius maximus. Cras ullamcorper commodo tortor. Curabitur feugiat dignissim urna, et ultrices dui vulputate et. Aliquam in ligula urna. Maecenas vel vulputate justo, eget cursus nunc. Integer pellentesque, lorem et vehicula faucibus, sapien enim gravida ex, et sagittis ligula ipsum eu urna. Phasellus cursus justo eget lacus pretium, id fringilla ligula pretium. Donec suscipit, libero non ornare vehicula, est erat porta sapien, eget congue sem nisi vel orci. Nunc sollicitudin fermentum ante, ac porttitor lacus commodo id. Vestibulum congue dui est, sed laoreet velit ultricies quis.
+              This page outlines how we handle your data when you use our services.
             </p>
           </div>
         </div>
@@ -24,11 +24,11 @@ export default function About() {
       <div className="flex justify-center p-2 py-5">
         <div className="flex flex-col md:w-5/6 lg:w-3/4 xl:w-1/2 justify-between">
             <h2 className="text-3xl font-bold text-primary mb-5 px-6 dark:text-gray-300">
-                A subheading<br />
+              What we learn about you<br />
             </h2>
           <div className="w-100 p-6 card dark:bg-gray-800">
             <p className="text-lg text-secondary dark:text-gray-300">
-                Curabitur id posuere ante, eu dapibus dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque quam quam, molestie eget ante non, fringilla pharetra enim. Donec id turpis ac dolor tempus scelerisque. Pellentesque venenatis metus vitae tellus gravida, laoreet imperdiet orci euismod. Sed nunc eros, hendrerit eget rhoncus a, mattis non ligula. In hac habitasse platea dictumst. Curabitur elementum dictum mi, sit amet iaculis odio semper a. Quisque neque lacus, faucibus nec tempor sed, venenatis quis mi. Aenean consectetur a ligula sit amet laoreet. Pellentesque a suscipit orci. Vestibulum cursus id nulla non pretium. Etiam posuere elit eget feugiat volutpat. Duis nec interdum turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti.
+              We value your privacy and strive to keep your information secure. When you use our service, we only collect your IP address, which is utilized solely for improving our service and ensuring its security. We do not retain any other identifying information on our servers or logsWe value your privacy and strive to keep your information secure. When you use our service, we may collect information such as your IP address, hardware information, software  information and file names. This data is utilized solely for improving our service and for service security. We do not retain any other identifying information on our servers or logs, and the logs are deleted after a short period. If you are detected as misusing our service, you may be blocked from accessing it. We do not share your information with any third parties.
             </p>
           </div>
         </div>
@@ -37,11 +37,24 @@ export default function About() {
       <div className="flex justify-center p-2 py-5">
         <div className="flex flex-col md:w-5/6 lg:w-3/4 xl:w-1/2 justify-between">
             <h2 className="text-3xl font-bold text-primary mb-5 px-6 dark:text-gray-300">
-                A subheading<br />
+              Image Uploads<br />
             </h2>
           <div className="w-100 p-6 card dark:bg-gray-800">
             <p className="text-lg text-secondary dark:text-gray-300">
-                Ut suscipit lorem vel sodales sodales. Vivamus dapibus at odio vel finibus. Curabitur non tempor orci. In fermentum purus at odio malesuada, at lobortis mauris ullamcorper. Donec varius sit amet ligula at sollicitudin. Nam auctor dolor in eros aliquet, sit amet condimentum nulla consequat. Praesent rutrum turpis nisl, ac vulputate erat congue quis. Maecenas placerat convallis nunc, quis aliquam diam blandit nec. Proin lobortis at urna quis dignissim. Curabitur at quam est. Nullam augue ligula, ultrices elementum lectus eget, tincidunt euismod enim. Donec pretium, tortor sed feugiat luctus, orci tortor imperdiet felis, vel faucibus velit arcu ac neque.
+              Regarding the images you upload: they are essential for the functionality of our service. However, rest assured that they are only temporarily stored on our servers during your session. Once your session ends, these images are promptly deleted, and we do not maintain any copies of them. If you upload a PDF for selecting a page to use, this PDF is removed from the server right after confirming. We do not perform any analysis on the content of the files you upload, and we do not share them with any third parties.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center p-2 py-5">
+        <div className="flex flex-col md:w-5/6 lg:w-3/4 xl:w-1/2 justify-between">
+            <h2 className="text-3xl font-bold text-primary mb-5 px-6 dark:text-gray-300">
+                Updating This Page<br />
+            </h2>
+          <div className="w-100 p-6 card dark:bg-gray-800">
+            <p className="text-lg text-secondary dark:text-gray-300">
+            From time to time, we may update this Privacy Policy to reflect changes in our practices or to comply with legal requirements. Any updates will be promptly reflected on this page, and we encourage you to review this Privacy Policy periodically for any updates. Last update was on 2024-04-XX.
             </p>
           </div>
         </div>
