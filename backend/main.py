0a379476bd888142301f51b9c2c11806c8fdc6ff
Trivinyx <tom.a.s.myre@gmail.com>
2024-03-26 17:08:50 +0100

    Renamed env and added flags to py script
    py script has --port and --mode

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,7 @@ def main():
     host="0.0.0.0"
     parser = argparse.ArgumentParser()
     parser.add_argument("-p",'--port', type=int, help='Port to run the server on')
+    parser.add_argument("-M",'--mode', type=str, help="Mode to run the server in, either 'dev' or 'prod'", default='dev')
     args = parser.parse_args()
     if args.port:
         port = args.port
@@ -23,11 +24,15 @@ def main():
             except:
                 pass
     import img2mapAPI.Img2mapAPI as Img2mapAPI
-    #run the uvicorn server
-    config = uvicorn.Config(Img2mapAPI.app, host=host, port=port, log_level="info")
-    server = uvicorn.Server(config)
-    server.run()
+    #check if the mode is production
+    if args.mode == 'prod':
+        print("Running in production mode")
+        uvicorn.run(Img2mapAPI.app, host=host, port=port)
+    else:
+        print("Running in development mode")
+        config = uvicorn.Config(Img2mapAPI.app, host=host, port=port, log_level="info", reload=True, reload_dirs=["img2mapAPI"])
+        server = uvicorn.Server(config)
+        server.run()
     
-
 if __name__ == '__main__':
     main()
